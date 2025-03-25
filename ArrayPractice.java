5803194fbf9db1a4c829eafc718d398619034ad9
natanojjj <32781825+natanojjj@users.noreply.github.com>
2025-03-25 00:39:10 -0500

    Section 2: Array Return Types

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArrayPractice{
     public static void main(String[] args){
         //Section 1: Array Creation
@@ -5,5 +7,18 @@ public class ArrayPractice{
         double[] doubleArray = {1.1, 2.2, 3.3};
         boolean[] booleanArray = {true, false, true};
         char[] charArray = {'a','b','c'};
+
+        System.out.println(getTwoNumbers());
+    
+    }
+    //Section 2: Array Return Types
+    public static int[] getTwoNumbers(){ 
+        return new int[] {5, 10}; //Anonymous Object
+    }
+    public static boolean[] getThreeBools(){
+        return new boolean[] {true,false,true};
+    }
+    public static double[] getTwoDoubles(){
+        return new double[] {1.5, 2.5};
     }
 }
